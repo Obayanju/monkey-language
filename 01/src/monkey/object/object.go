92659384965486a5d6cb37c823332e3f9000a681
Dami <obayanjud@gmail.com>
2019-08-21 11:40:50 -0600

    Foundations of the Object System
    Its currently capable of representing boolean, integer, and null values

---
+++ 01/src/monkey/object/object.go
@@ -0,0 +1,50 @@
+package object
+
+import "fmt"
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+type Boolean struct {
+	Value bool
+}
+
+type Null struct{}
+
+func (n *NUll) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *NUll) Inspect() string {
+	return "null"
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
