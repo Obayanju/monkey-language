75050efd82192cb3ada667ba10d943c5edd5cbe1
Dami <obayanjud@gmail.com>
2019-08-23 00:52:49 -0600

    Add support for boolean literals and null

--- 01/src/monkey/evaluator/evaluator.go
+++ 01/src/monkey/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"monkey-lang/01/src/monkey/object"
 )
 
+var (
+	NULL  = &object.NULL{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -17,9 +23,17 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
-
 	return nil
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
