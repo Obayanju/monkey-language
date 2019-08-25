a48f6e2ad5a44a33785a97adcb6beef5305ff177
Dami <obayanjud@gmail.com>
2019-08-25 00:51:09 -0600

    Functions and Function Calls
    support for functions, higher-order functions, closures, first class
    functions :)

--- 01/src/monkey/object/object.go
+++ 01/src/monkey/object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey-lang/01/src/monkey/ast"
+	"strings"
+)
 
 const (
 	INTEGER_OBJ      = "INTEGER"
@@ -8,7 +13,14 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
 
 type Error struct {
 	Message string
@@ -34,6 +46,26 @@ type Boolean struct {
 }
 
 type Null struct{}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 
