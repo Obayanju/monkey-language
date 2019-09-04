cacb87449a25fa37ee5a6ac414077c45aed014d8
Dami <obayanjud@gmail.com>
2019-09-03 23:18:40 -0600

    Print to console

--- 01/src/monkey/evaluator/builtins.go
+++ 01/src/monkey/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey-lang/01/src/monkey/object"
+import (
+	"fmt"
+	"monkey-lang/01/src/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -21,6 +24,16 @@ var builtins = map[string]*object.Builtin{
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
 			}
+		},
+	},
+
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 
